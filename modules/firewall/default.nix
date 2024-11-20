4363defbd3c6f2c63f6da73081537d550d6a227b
derkalaender <git@derkalaender.de>
2024-11-20 15:43:49 +0100

    Add additional Debian VMs

--- modules/firewall/default.nix
+++ modules/firewall/default.nix
@@ -17,6 +17,6 @@
   networking.proxy = {
     httpProxy = "http://proxy.cit.tum.de:8080";
     httpsProxy = config.networking.proxy.httpProxy;
-    noProxy = "localhost,127.0.0.1,192.168.0.0/16";
+    noProxy = "localhost,127.0.0.1,192.168.0.0/16,psa-team01.cit.tum.de,psa-team02.cit.tum.de,psa-team03.cit.tum.de,psa-team04.cit.tum.de,psa-team05.cit.tum.de,psa-team06.cit.tum.de,psa-team07.cit.tum.de,psa-team08.cit.tum.de,psa-team09.cit.tum.de,psa-team10.cit.tum.de";
   };
 }
