059907462955560c018097a166d7570070006ef2
derkalaender <git@derkalaender.de>
2024-12-02 00:48:44 +0100

    Add user dir to webserver

--- modules/webserver/default.nix
+++ modules/webserver/default.nix
@@ -2,8 +2,17 @@
 let
   cfg = config.psa.webserver;
 
-  sslKey = ./apache-selfsigned.key;
-  sslCert = ./apache-selfsigned.crt;
+  # Document roots, world-readable
+  kumoSite = ./sites/kumo;
+  wwwSite = ./sites/www;
+  webSite = ./sites/web;
+
+  # Gemeine SSL Attribute
+  sslAttr = {
+    forceSSL = true; # SSL redirect
+    sslServerKey = ./apache-selfsigned.key;
+    sslServerCert = ./apache-selfsigned.crt;
+  };
 in
 {
   options = {
@@ -14,26 +23,40 @@ in
     services.httpd = {
       enable = true;
       enablePHP = true;
+      extraModules = [
+        "userdir"
+      ];
 
       # Hier definieren wir alle Hosts die erreichbar sein sollen. Lauschen alle auf 0.0.0.0
       virtualHosts = {
         "kumo.psa-team06.cit.tum.de" = {
-          forceSSL = true;
-          sslServerKey = sslKey;
-          sslServerCert = sslCert;
-        };
+          documentRoot = kumoSite;
+          # User Websites
+          extraConfig =
+            ''
+              # Adapted from https://github.com/NixOS/nixpkgs/blob/7e1ca67996afd8233d9033edd26e442836cc2ad6/nixos/modules/services/web-servers/apache-httpd/default.nix#L249-L262
+              UserDir .html-data
+              UserDir disabled root
+              <Directory "/home/*/.html-data">
+                AllowOverride FileInfo AuthConfig Limit Indexes
+                Options MultiViews Indexes SymLinksIfOwnerMatch IncludesNoExec
+                <Limit GET POST OPTIONS>
+                  Require all granted
+                </Limit>
+                <LimitExcept GET POST OPTIONS>
+                  Require all denied
+                </LimitExcept>
+              </Directory>
+            '';
+        } // sslAttr;
 
         "www.psa-team06.cit.tum.de" = {
-          forceSSL = true;
-          sslServerKey = sslKey;
-          sslServerCert = sslCert;
-        };
+          documentRoot = wwwSite;
+        } // sslAttr;
 
         "web.psa-team06.cit.tum.de" = {
-          forceSSL = true;
-          sslServerKey = sslKey;
-          sslServerCert = sslCert;
-        };
+          documentRoot = webSite;
+        } // sslAttr;
       };
     };
 
