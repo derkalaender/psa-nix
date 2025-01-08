18cff436fb43429653b0894242bfea531c585b9b
derkalaender <git@derkalaender.de>
2025-01-08 18:10:46 +0100

    A bit of ldap

--- modules/ldap/server.nix
+++ modules/ldap/server.nix
@@ -1,6 +1,11 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 let
   cfg = config.psa.ldap;
+
+  # suffix for the database carrying all data entries
+  baseDN = "dc=team06,dc=psa,dc=cit,dc=tum,dc=de";
+  rootName = "admin";
+  rootPw = "{SSHA}REHwlTzaAcFssX+EYQwJ9rp0w9M80QMN";
 in
 {
   options = {
@@ -12,12 +17,48 @@ in
       enable = true;
 
       # Only allow secure connections
-      urlList = [ "ldapi:///" "ldaps:///" ];
+      urlList = [ "ldapi:///" "ldap:///" "ldaps:///" ];
 
       # Recursive configuration in on-line configuration (OLC) format
+      # Starts with dn: cn=config
+      # `attrs` are attributes for the current entry
+      # `children` contains child entries
+      # `includes` adds other ldif files
       settings = {
         attrs = {
-          # TODO TLS
+          # activate more verbose logging
+          olcLogLevel = [ "stats" "conns" "config" "acl" ];
+        };
+
+        children = {
+          "cn=schema".includes = [
+            # required
+            "${pkgs.openldap}/etc/schema/core.ldif"
+          ];
+
+          # Database
+          "olcDatabase={1}mdb".attrs = {
+            objectClass = [ "olcDatabaseConfig" "olcMdbConfig" ];
+            olcDatabase = "{1}mdb";
+
+            # Base DN for all entries in the database
+            olcSuffix = baseDN;
+
+            # Credentials for DN without access restrictions
+            # Meta, doesn't actually need to exist in the database
+            olcRootDN = "cn=${rootName},${baseDN}";
+            olcRootPW = rootPw;
+
+            # Directory to store the database in
+            olcDbDirectory = "/var/lib/openldap/data";
+
+            olcAccess = [
+              # allow root linux user complete access
+              ''{0}to *
+                  by dn.exact=uidNumber=0+gidNumber=0,cn=peercred,cn=external,cn=auth manage
+                  by * break''
+            ];
+          };
         };
       };
     };
