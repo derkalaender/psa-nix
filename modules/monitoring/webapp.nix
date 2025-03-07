d57c40aaaa6e2d73cbda9d74a2ad0ae08ab33881
derkalaender <git@derkalaender.de>
2025-03-07 19:07:36 +0100

    typo

--- modules/monitoring/webapp.nix
+++ modules/monitoring/webapp.nix
@@ -11,7 +11,7 @@ in {
         params.module = ["http_200"];
         static_configs = [
           {
-            targets = cfg.vms.webserverURLs;
+            targets = cfg.vms.webappURLs;
           }
         ];
         # This is needed so we can have the targets be the actual VMs but route all requests to blackbox locally
